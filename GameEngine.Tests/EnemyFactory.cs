0bcc57686f13fba42d38766ad4d4bda698331181
Iain Stewart <iain.stewart08@gmail.com>
2022-02-23 21:17:26 +0000

    Add test for argument null exception

---
+++ GameEngine.Tests/EnemyFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class EnemyFactory
+    {
+        [Fact]
+        public void EnemyFactory_ShouldNotAllowNullName()
+        {
+            GameEngine.EnemyFactory sut = new GameEngine.EnemyFactory();
+
+            Assert.Throws<ArgumentNullException>(() => sut.Create(null));
+        }
+    }
+}
