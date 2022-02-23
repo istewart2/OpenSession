ff08802bfdd31eafd3303a14907bab250db81589
Iain Stewart <iain.stewart08@gmail.com>
2022-02-23 23:03:19 +0000

    Add autofixture sample for complex object graph

---
+++ AutoFixtureDemo.Tests/OrderTest.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture;
+using System;
+using Xunit;
+
+namespace AutoFixtureDemo.Tests
+{
+    public class OrderTest
+    {
+        [Fact]
+        public void ManualObjectGraphCreation()
+        {
+            Customer customer = new Customer()
+            {
+                CustomerName = "Mary Smith"
+            };
+
+            Order order = new Order(customer)
+            {
+                Id = 1,
+                OrderDate = DateTime.Now,
+                Items =
+            {
+                new OrderItem()
+                {
+                    ProductName = "TestProduct1",
+                    Quantity = 5
+                },
+                new OrderItem()
+                {
+                    ProductName = "TestProduct2",
+                    Quantity = 3
+                },
+            }
+            };
+
+            // act
+            // assert
+        }
+
+        [Fact]
+        public void AutoCreation()
+        {
+            var fixture = new Fixture();
+            var order = fixture.Create<Order>();
+
+            // act
+            // assert
+        }
+    }
+}
