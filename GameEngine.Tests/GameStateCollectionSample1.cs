2907ffa766212cd7cec9af073958491cb185cb89
Iain Stewart <iain.stewart08@gmail.com>
2022-02-23 22:11:59 +0000

    Add game state collection and use to share state across classes

---
+++ GameEngine.Tests/GameStateCollectionSample1.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]
+    [Trait("Category", "Collection Sample")]
+    public class GameStateCollectionSample1
+    {
+        private readonly GameStateFixture gameStateFixture;
+        private readonly ITestOutputHelper output;
+
+        public GameStateCollectionSample1(GameStateFixture gameStateFixture, ITestOutputHelper output)
+        {
+            this.gameStateFixture = gameStateFixture;
+            this.output = output;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+    }
+}
---
+++ GameEngine.Tests/GameStateCollectionSample2.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace GameEngine.Tests
+{
+    [Collection("GameState collection")]
+    // let's you group tests into groups which can be run independently
+    // run both classes together to check that the same instance is being used
+    [Trait("Category", "Collection Sample")]
+    public class GameStateCollectionSample2
+    {
+        private readonly GameStateFixture gameStateFixture;
+        private readonly ITestOutputHelper output;
+
+        public GameStateCollectionSample2(GameStateFixture gameStateFixture, ITestOutputHelper output)
+        {
+            this.gameStateFixture = gameStateFixture;
+            this.output = output;
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            output.WriteLine($"GameState ID={gameStateFixture.State.Id}");
+        }
+    }
+}
