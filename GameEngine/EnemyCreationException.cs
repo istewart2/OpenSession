7991aa9efbc724a3d9c53c2365faa9a5176d552e
Iain Stewart <iain.stewart08@gmail.com>
2022-02-23 20:33:04 +0000

    Remove RequestedEnemyName property

--- GameEngine/EnemyCreationException.cs
+++ GameEngine/EnemyCreationException.cs
@@ -4,11 +4,8 @@ namespace GameEngine
 {
     public class EnemyCreationException : Exception
     {
-        public EnemyCreationException(string message, string enemyName) : base(message)
+        public EnemyCreationException(string message) : base(message)
         {
-            RequestedEnemyName = enemyName;
         }
-
-        public string RequestedEnemyName { get; private set; }
     }
 }
