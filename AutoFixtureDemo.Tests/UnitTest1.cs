d702cd3cfc3e394f0202658ed0a06ac4cb7c6616
Iain Stewart <iain.stewart08@gmail.com>
2022-02-23 22:42:15 +0000

    Add comments about evaluating library

--- AutoFixtureDemo.Tests/UnitTest1.cs
+++ AutoFixtureDemo.Tests/UnitTest1.cs
@@ -4,6 +4,17 @@ namespace AutoFixtureDemo.Tests
 {
     public class UnitTest1
     {
+        // remember to add Autofixture package reference
+
+        // autofixture github: https://github.com/AutoFixture/AutoFixture
+        // "easily create test data for your unit tests"
+        // how would we evaluate whether to use a library?
+
+        // num of stars, open issues, recent commits, documentation, community, code quality
+        // does it do something that would be complex / time consuming to implement ourselves
+        // num of downloads - https://www.nuget.org/profiles/AutoFixture/
+
+        // quick start guide: https://autofixture.github.io/docs/quick-start/
         [Fact]
         public void Test1()
         {
